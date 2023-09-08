545693861ca9df0bfaa9cc37be96e65a65021587
William Hearn <williamhearn17@gmail.com>
2023-09-08 01:18:56 +0000

    add consecutive counting blablablabla

--- timeline_generator.py
+++ timeline_generator.py
@@ -18,7 +18,8 @@ def load_config(filename="config.json"):
 
 
 def get_event_info(operation, config):
-    return config.get(operation, {"name": "Unknown Event", "description": "No description for this event.", "source": "Unknown Source"})
+    return config.get(operation, {"name": "Unknown Event", "description": "No description for this event.",
+                                  "source": "Unknown Source"})
 
 
 def process_csv_file(filename):
@@ -54,34 +55,64 @@ def get_event_name(operation, event_config):
 def generate_timeline(events):
     timeline = ""
     current_date = None
+    consecutive_operation_count = 1
+    last_operation = ""
+    start_time_of_consecutive_operations = None
 
     for event in events:
         date, operation, ip_address, extended_details = event
-        event_name = get_event_name(operation, config["events"])
+
+        if last_operation == "builtin\\date change":
+            last_operation = operation
 
         # If we're on a new day, add the date header.
         if current_date != date.date():
             timeline += "{}\n".format(date.strftime('%B %d, %Y'))
             current_date = date.date()
 
-        # Directly format each operation as its own line
-        timeline += format_operation(date, operation, ip_address, extended_details, config["events"])
+            timeline += format_operation(date, operation, ip_address, extended_details, config["events"],
+                                         consecutive_operation_count, start_time_of_consecutive_operations)
+            consecutive_operation_count = 1
+            start_time_of_consecutive_operations = date
+            last_operation = "builtin\\date change"
+
+        # If we are on the same day/same kind of event as last, don't add, just count
+        elif current_date == date.date() and last_operation == operation and not extended_details:
+            consecutive_operation_count += 1
+
+        # If we are on same day but different op, stop count and add op to timeline
+        elif current_date == date.date() and (last_operation != operation or extended_details):
+            timeline += format_operation(date, operation, ip_address, extended_details, config["events"],
+                                         consecutive_operation_count, start_time_of_consecutive_operations)
+
+            consecutive_operation_count = 1
+            start_time_of_consecutive_operations = date
+            last_operation = operation
+
+    # Ensure to add the last entry if needed
+    if consecutive_operation_count > 1 or (
+            consecutive_operation_count == 1 and last_operation != "builtin\\date change"):
+        timeline += format_operation(date, last_operation, ip_address, extended_details, config["events"],
+                                     consecutive_operation_count, start_time_of_consecutive_operations)
 
     return timeline
 
 
-def format_operation(date, operation, ip_address, extended_details, config):
-    time_str = date.strftime('%H:%M')
+def format_operation(date, operation, ip_address, extended_details, config, operation_count, start_time):
+    if operation_count > 1:
+        time_str = "{}-{}".format(start_time.strftime('%H:%M'), date.strftime('%H:%M'))
+    else:
+        time_str = date.strftime('%H:%M')
 
     event_info = get_event_info(operation, config)
     combined_name = "[{}] {}".format(event_info["source"], event_info["name"])
-    description = event_info["description"].format(IP=ip_address)
+    description = event_info["description"].format(IP=ip_address, count=operation_count)
 
     # If extended details exist for the event, append it to the description.
     if extended_details:
         description += " ({})".format(extended_details)
 
-    return "{}\t{}: {}\n".format(time_str, combined_name, description)
+    return "{}\t{}\t{}\n".format(time_str, combined_name, description)
 
 
 if __name__ == "__main__":
