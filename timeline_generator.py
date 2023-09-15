48090f6e4f4b5952f11ff52fb3d3525f76cac428
William Hearn <williamhearn17@gmail.com>
2023-09-15 20:22:16 +0000

    Refactor timeline generator and add file output
    - Enhanced error handling to capture potential exceptions.
    - Introduced logging for better diagnostic and error messages.
    - Made string formatting consistent using f-strings.
    - Passed configuration to functions instead of using globals.
    - Removed clipboard functionality; now saves output to timeline.txt.

--- timeline_generator.py
+++ timeline_generator.py
@@ -1,38 +1,40 @@
 import csv
-from datetime import datetime
-import pyperclip
-import argparse
 import json
+from datetime import datetime
+import argparse
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 DATE_FORMAT_24HR = '%m/%d/%y %H:%M'
 DATE_FORMAT_12HR = '%m/%d/%y %I:%M'
 
 
 def load_config(filename="config.json"):
+    """Load and return events from the config file."""
     try:
         with open(filename, 'r') as f:
             config = json.load(f)
-            return config['events']  # Dive into the 'events' key
-    except FileNotFoundError:
-        print(f"Config file {filename} not found!")
+            return config.get('events', {})  # Get the 'events' key or default to an empty dictionary
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logger.error(f"Error loading config file {filename}: {e}")
         exit(1)
 
 
 def get_event_info(event_name, config):
-    details = config.get(event_name)  # Directly look up the event using its name.
-    if details:
-        return details
-    # If no matching event details found, return a default.
-    return {
+    """Retrieve event details from config."""
+    return config.get(event_name, {
         "name": "Unknown Event",
         "description": "No description for this event.",
         "source": "Unknown Source"
-    }
+    })
 
 
 def process_csv_file(filename):
+    """Process a CSV file and return a sorted list of events."""
     events = []
-
     try:
         with open(filename, 'r') as f:
             reader = csv.DictReader(f)
@@ -46,55 +48,55 @@ def process_csv_file(filename):
                 operation = row["Event"]
                 args = {k.casefold(): v for k, v in row.items() if k not in ["Time", "Event"]}
                 events.append((date, operation, args))
-    except FileNotFoundError:
-        print(f"CSV file {filename} not found!")
+    except (FileNotFoundError, csv.Error) as e:
+        logger.error(f"Error processing CSV file {filename}: {e}")
         exit(1)
 
     events.sort(key=lambda x: x[0])
     return events
 
 
-def get_event_name(operation, event_config):
-    return event_config.get(operation, {}).get("name", "Unknown Event")
-
-
-def generate_timeline(events):
-    timeline = ""
-    current_date = None
-
-    for event in events:
-        date, operation, args = event
-
-        # If we're on a new day, add the date header.
-        if current_date != date.date():
-            timeline += "{}\n".format(date.strftime('%B %d, %Y'))
-            current_date = date.date()
-
-        timeline += format_operation(date, operation, args, config)
-
-    return timeline
-
-
 def format_operation(date, operation, args, config):
+    """Return a formatted operation string."""
     time_str = date.strftime('%H:%M')
     event_info = get_event_info(operation, config)
-    combined_name = "[{}] {}".format(event_info["source"], event_info["name"])
+    combined_name = f"[{event_info['source']}] {event_info['name']}"
 
     # This line dynamically inserts args.
     formatted_args = {k.casefold(): v for k, v in args.items()}
     description = event_info["description"].format(**formatted_args)
 
-    return "{}\t{}\t{}\n".format(time_str, combined_name, description)
+    return f"{time_str}\t{combined_name}\t{description}\n"
+
+
+def generate_timeline(events, config):
+    """Generate a timeline from a list of events."""
+    timeline = ""
+    current_date = None
+
+    for event in events:
+        date, operation, args = event
+        if current_date != date.date():
+            timeline += f"{date.strftime('%B %d, %Y')}\n"
+            current_date = date.date()
+        timeline += format_operation(date, operation, args, config)
+
+    return timeline
+
+
+def save_to_file(timeline, filename="timeline.txt"):
+    """Save the generated timeline to a file."""
+    with open(filename, 'w') as f:
+        f.write(timeline)
+    logger.info(f"Timeline saved to {filename}!")
 
 
 if __name__ == "__main__":
-    config = load_config()
-
-    parser = argparse.ArgumentParser(description="Generate a timeline from a CSV file and copy it to the clipboard.")
+    parser = argparse.ArgumentParser(description="Generate a timeline from a CSV file and save it to a text file.")
     parser.add_argument("filename", help="Path to the CSV file.")
     args = parser.parse_args()
 
+    config = load_config()
     events = process_csv_file(args.filename)
-    timeline = generate_timeline(events)
-    pyperclip.copy(timeline)
-    print("Timeline copied to clipboard!")
+    timeline = generate_timeline(events, config)
+    save_to_file(timeline)
